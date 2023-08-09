99a556bc2e0e75954a0b0116e6798ba4f1326833
bootvue <bootvue@gmail.com>
2023-08-09 14:45:11 +0800

    crx hmr热更新

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,1 +1,8 @@
-console.log('background...')
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+
+    // dev hmr
+    if ('development' === import.meta.env.MODE && request.msg == 'reload') {
+        chrome.runtime.reload()
+    }
+
+})
--- src/content_script/index.ts
+++ src/content_script/index.ts
@@ -1,1 +1,4 @@
-console.log('content script...')
+if ('development' === import.meta.env.MODE) {
+// dev hmr
+    chrome.runtime.sendMessage({msg: 'reload'})
+}
